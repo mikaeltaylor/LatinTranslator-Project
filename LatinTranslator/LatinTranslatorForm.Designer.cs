6e57cb7bce4f6d9f774e87b0c36f29a21e183606
mikaeltaylor <mikael.e.taylor@gmail.com>
2016-05-16 07:49:47 -0500

    Latin Translator Commit

---
+++ LatinTranslator/LatinTranslatorForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace LatinTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSinisterLeft = new System.Windows.Forms.Button();
+            this.btnDexterRight = new System.Windows.Forms.Button();
+            this.btnMediumCenter = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSinisterLeft
+            // 
+            this.btnSinisterLeft.Location = new System.Drawing.Point(12, 33);
+            this.btnSinisterLeft.Name = "btnSinisterLeft";
+            this.btnSinisterLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnSinisterLeft.TabIndex = 0;
+            this.btnSinisterLeft.Text = "Sinister";
+            this.btnSinisterLeft.UseVisualStyleBackColor = true;
+            this.btnSinisterLeft.Click += new System.EventHandler(this.btnSinisterLeft_Click);
+            // 
+            // btnDexterRight
+            // 
+            this.btnDexterRight.Location = new System.Drawing.Point(103, 33);
+            this.btnDexterRight.Name = "btnDexterRight";
+            this.btnDexterRight.Size = new System.Drawing.Size(75, 23);
+            this.btnDexterRight.TabIndex = 1;
+            this.btnDexterRight.Text = "Dexter";
+            this.btnDexterRight.UseVisualStyleBackColor = true;
+            this.btnDexterRight.Click += new System.EventHandler(this.btnDexterRight_Click);
+            // 
+            // btnMediumCenter
+            // 
+            this.btnMediumCenter.Location = new System.Drawing.Point(197, 33);
+            this.btnMediumCenter.Name = "btnMediumCenter";
+            this.btnMediumCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnMediumCenter.TabIndex = 2;
+            this.btnMediumCenter.Text = "Medium";
+            this.btnMediumCenter.UseVisualStyleBackColor = true;
+            this.btnMediumCenter.Click += new System.EventHandler(this.btnMediumCenter_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblResult.Location = new System.Drawing.Point(0, 80);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(283, 16);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Press one of the buttons.";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnMediumCenter);
+            this.Controls.Add(this.btnDexterRight);
+            this.Controls.Add(this.btnSinisterLeft);
+            this.Name = "Form1";
+            this.Text = "Latin Translator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSinisterLeft;
+        private System.Windows.Forms.Button btnDexterRight;
+        private System.Windows.Forms.Button btnMediumCenter;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ LatinTranslator/LatinTranslatorForm.cs
@@ -0,0 +1,46 @@
+﻿// Program: Latin Translator
+// Developer: Mikael Taylor
+// Date: January 21, 2016
+// Description: This program displays translation results from Latin to English for the words Sinister, Dexter, and Center.
+//              It does so with the use of 3 buttons that translate the latin words that are displayed on the button into
+//              their respective English translations.
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace LatinTranslator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {//begin Form1
+            InitializeComponent();//Default constructor.
+        }//end Form1
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {//load the form
+            lblResult.Text = "Press one of the buttons.";//Default lblResult value when no button has been clicked.
+        }//end load form
+
+        private void btnSinisterLeft_Click(object sender, EventArgs e)//btnSinisterLeft's method.
+        {//begin btnSinisterLeft click
+            lblResult.Text = "Left";//When the Sinister button is clicked, change the label's text to Left.
+        }//end btnSinisterLeft click
+
+        private void btnDexterRight_Click(object sender, EventArgs e)
+        {//begin btnDexterRight click
+            lblResult.Text = "Right";//When the Dexter button is clicked, change the label's text to Right.
+        }//end btnDexterRight click
+
+        private void btnMediumCenter_Click(object sender, EventArgs e)
+        {//begin btnMediumCenter click
+            lblResult.Text = "Center";//When the Medium button is clicked, change the label's text to Center.
+        }//end btnMediumCenter click
+    }//close Form1
+}//end class
